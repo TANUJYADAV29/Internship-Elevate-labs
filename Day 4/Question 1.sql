a5c3ddc5be04b0dc8dd31e7ec4b890be675b6bc0
TANUJYADAV29 <142374376+TANUJYADAV29@users.noreply.github.com>
2025-09-28 21:59:24 +0530

    Add files via upload

---
+++ Day 4/Question 1.sql
@@ -0,0 +1,5 @@
+use pizzahut;
+
+-- Retrieve the total number of orders placed
+
+select count(order_id) as total_orders from orders;
---
+++ Day 4/Question 5.sql
@@ -0,0 +1,11 @@
+-- List the top 5 most ordered pizza types along with their quantities.
+
+SELECT 
+    pizzas.size,
+    COUNT(orders_details.order_details_id) AS order_count
+FROM
+    pizzas
+        JOIN
+    orders_details ON pizzas.pizza_id = orders_details.pizza_id
+GROUP BY pizzas.size
+ORDER BY order_count DESC;
